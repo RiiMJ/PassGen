ea942f375d94f8b7871f6adb6747dacfa99af2ee
RiiMJ <99528014+RiiMJ@users.noreply.github.com>
2022-03-20 12:44:04 +0530

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,67 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    ui->pushButton->setStyleSheet("background-color: darkCyan");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    std::string charSet(""),
+                output("");
+
+    if (ui->checkBox_5->isChecked())
+    {
+        charSet = ui->lineEdit->text().toStdString();
+    }
+    else
+    {
+        if (ui->checkBox_1->isChecked())
+        {
+            charSet+="abcdefghijklmnopqrstuvwxyz";
+        }
+        if (ui->checkBox_2->isChecked())
+        {
+            charSet+="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if (ui->checkBox_3->isChecked())
+        {
+            charSet+="1234567890";
+        }
+        if (ui->checkBox_4->isChecked())
+        {
+            charSet+="!@#$%^&*()";
+        }
+    }
+
+    if (charSet!="")
+    {
+        for (int i=0; i<ui->spinBox->value(); ++i)
+        {
+            output+=charSet.at(rand()%(int)charSet.length());
+        }
+        ui->plainTextEdit->appendPlainText(output.c_str());
+    }
+}
+
+
+
+void MainWindow::on_pushButton_pressed()
+{
+    ui->pushButton->setStyleSheet("background-color: rgb(0,114,114)");
+}
+void MainWindow::on_pushButton_released()
+{
+    ui->pushButton->setStyleSheet("background-color: darkCyan");
+}
+
